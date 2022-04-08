8245d9720ae9b270581007d8b1650aecd281fb2a
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2022-04-08 13:36:39 +0200

    n1
    n1

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -13,11 +13,13 @@ int potega_liczby(int liczba, int potega){
 
 int main(){
 	
-	int wyn,wyn1;
+	int wyn,wyn1,w2;
 	
 	wyn = potega_liczby(4,6);
 	wyn1 = potega_liczby(2,3);
-
-	printf(" %d ", wyn);
+	w2 = potega_liczby(33,2);
+	printf(" %d \n", wyn);
+	printf(" %d \n", wyn1);
+	printf(" %d ", w2);
 }
 
