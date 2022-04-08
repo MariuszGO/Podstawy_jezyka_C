436c479f5f2860cfd3a7c4f8c83d9bc413b2d278
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2022-04-08 14:55:27 +0200

    kasd
    sdds

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define pi 3.14
 int potega_liczby(int liczba, int potega){
 	int i,w;
 	w= liczba;
@@ -10,16 +10,29 @@ int potega_liczby(int liczba, int potega){
 	return w;
 }
 
+double pole_kola(int r){
+		double wy;
+		wy = pi * r * r;	
+		return wy;
+}
+
+//https://github.com/MariuszGO/podstawy_jezyka_c
+
+
 
 int main(){
 	
 	int wyn,wyn1,w2;
+	double w;
 	
 	wyn = potega_liczby(4,6);
 	wyn1 = potega_liczby(2,3);
 	w2 = potega_liczby(33,2);
-	printf(" %d \n", wyn);
-	printf(" %d \n", wyn1);
-	printf(" %d ", w2);
+//	printf(" %d \n", wyn);
+//	printf(" %d \n", wyn1);
+//	printf(" %d \n", w2);
+	
+	w = pole_kola(2);
+	printf(" %lf ", w);
 }
 
